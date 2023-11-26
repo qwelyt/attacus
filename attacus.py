4b58eb17893836d1ac57834b1d29174068aec94c
qwelyt <qwelyt@users.noreply.github.com>
2023-11-26 23:49:13 +0100

    Figure out how to split pieces

--- attacus.py
+++ attacus.py
@@ -1,6 +1,10 @@
 # %%
 from ocp_vscode import show, show_object, reset_show, set_port, set_defaults, get_defaults, Camera
 set_port(3939)
+set_defaults(
+    reset_camera=Camera.KEEP
+    ,measure_tools=True
+    )
 
 import build123d as bd
 import pro_micro_c
@@ -8,6 +12,7 @@ import diode_1n4148
 import copy, itertools, functools
 from typing import TypeVar
 T = TypeVar('T')
+#%%
 w = 19.05
 l = 19.05
 cut_size = 14.07
@@ -20,7 +25,7 @@ cherry_switch = bd.import_step("cherry_mx.stp").rotate(bd.Axis.X, 90).move(bd.Lo
 keycap = bd.import_step("DSA_1u.step").rotate(bd.Axis.X, 90).move(bd.Location((0,0,8.7)))
 dil_socket = bd.import_step("DIL_socket_24pins.step")
 promicro = pro_micro_c.pro_micro().rotate(bd.Axis.Z, 180).move(bd.Location((0,18,1)))
-diode = diode_1n4148.diode_1n4148().rotate(bd.Axis.X, 90)
+diode = diode_1n4148.diode_1n4148(leg_diameter=1).rotate(bd.Axis.X, 90)
 #%%
 def col(thing, rows:int=3):
     _locations = bd.GridLocations(w,l,1,rows)
@@ -68,13 +73,22 @@ def outline():
     skt=skt.sketch+bd.make_hull(wires)
     return skt
 
-def plate():
+def plate(thickness=2):
     ol = outline()
+    with bd.BuildSketch() as cross:
+        bd.add(bd.Rectangle(cut_size+2,4))
+        bd.add(bd.Rectangle(4, cut_size+2))
+        bd.add(bd.Rectangle(cut_size, cut_size))
+
     with bd.BuildPart() as prt:
-        with bd.BuildSketch() as skt:
+        with bd.BuildSketch():
+            bd.add(ol)
+            bd.add(key_locations(cross.sketch), mode=bd.Mode.SUBTRACT)
+        bd.extrude(amount=thickness-1)
+        with bd.BuildSketch(bd.Location((0,0,prt.part.bounding_box().size.Z))):
             bd.add(ol)
             bd.add(key_locations(bd.Rectangle(cut_size, cut_size)), mode=bd.Mode.SUBTRACT)
-        bd.extrude(amount=2)
+        bd.extrude(amount=1)
     return prt.part
 
 def top_case(thickness:float=4.0, height:float=20.0, lip_height:float=4):
@@ -127,29 +141,8 @@ def bottom_case(thickness:float=4.0, height:float=4.0):
             bd.offset(amount=-6, kind=bd.Kind.INTERSECTION, mode=bd.Mode.SUBTRACT)
         bd.extrude(amount=height)
     return prt.part
-tc = top_case()
-bc = bottom_case()
-show(
-   tc
-   , bc
-    #, plate().translate((0,0,8.8))
-    ,reset_camera=Camera.KEEP
-)
-#%%
-ol = outline()
-with bd.BuildPart() as p:
-    with bd.BuildSketch() as ks:
-        bd.add(ol)
-        bd.offset(amount=-2, kind=bd.Kind.INTERSECTION)
-        bd.offset(amount=-2, kind=bd.Kind.INTERSECTION, mode=bd.Mode.SUBTRACT)
 
-    bd.extrude(amount=4)
-
-
-show(ol,p.part.translate((0,0,1))
-,reset_camera=Camera.KEEP)
-#%%
-def diodes(alt=1):
+def diodes(alt=1,rows=3,groups=6):
     dbb = diode.bounding_box().size
     if alt==1:
         grid1 = bd.GridLocations(dbb.X+1, (dbb.Y)*1.5, 5,2)
@@ -182,25 +175,25 @@ def diodes(alt=1):
     elif alt==6:
         dide = diode.rotate(bd.Axis.Z, 90)
         dbb = dide.bounding_box().size
-        grid = bd.GridLocations(dbb.X+1, (dbb.Y+1)*3, 1,6)
+        grid = bd.GridLocations(dbb.X+1, (dbb.Y+1)*rows, 1,groups)
         group = bd.Part()+[copy.copy(dide).locate(loc) for loc in grid]
-        dds = [copy.copy(group).locate(bd.Location((1*i, (dbb.Y+1)*i, 0))) for i in range(3)]
+        dds = [copy.copy(group).locate(bd.Location((1*i, (dbb.Y+1)*i, 0))) for i in range(rows)]
         return bd.Part()+dds
 #%%
 with bd.BuildPart() as rods:
     with bd.BuildSketch():
         with bd.Locations((0,2.24)):
             with bd.GridLocations(16, 2.54, 2, 12):
-                bd.Circle(radius=0.55/2)
+                bd.Circle(radius=1/2)
     bd.extrude(amount=20)
         
 rods = rods.part.locate(bd.Location(promicro.center())).move(bd.Location((0,-2.54*2,-10)))
-show(
-    promicro
-    #,dil_socket
-    ,rods
-    ,reset_camera=Camera.KEEP
-)
+# show(
+#     promicro
+#     #,dil_socket
+#     ,rods
+#     ,reset_camera=Camera.KEEP
+# )
 # %%
 tc = top_case()
 tcbb = tc.bounding_box().size
@@ -228,17 +221,70 @@ plt_with_holes = plt-diodes_left-diodes_right-pm_pins
 #%%
 show(
     #bc.translate((0,0,-2))
-     plt_with_holes
+    plt_with_holes#.split(bd.Plane(bd.Plane.X))
     #, tc
     #, switches.translate((0,-1,plt.location.position.Z+pltbb.Z))
     #, caps.translate((0,0,plt.location.position.Z+pltbb.Z+cherry_switch.bounding_box().size.Z/3+0.5))
-    #,pm
+    ,pm
     # ,dil_socket.locate(bd.Location(pm.center()))
-    #,diodes_left
-    #,diodes_right
+    ,diodes_left
+    ,diodes_right
     ,reset_camera=Camera.KEEP
 )
 #%%
 plt_with_holes.export_stl(__file__.replace(".py","_plate.stl"))
 #%%
-tc.export_stl(__file__.replace(".py","_top_plate.stl"))
+tc.export_stl(__file__.replace(".py","_top_case.stl"))
+bc.export_stl(__file__.replace(".py","_bottom_case.stl"))
+#%%
+
+plate_right = bd.split(plt_with_holes, bd.Plane.YZ.offset(plt_with_holes.center().X), keep=bd.Keep.TOP)
+plate_left = bd.split(plt_with_holes, bd.Plane.YZ.offset(plt_with_holes.center().X), keep=bd.Keep.BOTTOM)
+show(
+    #bd.split(plt_with_holes, bd.Plane(plt_with_holes.faces().sort_by(bd.Axis.X)[-1]))#.offset(plt_with_holes.bounding_box().size.X/2))
+    #plt_with_holes
+    plate_right
+    ,plate_left
+    ,bone
+    ,reset_camera=Camera.KEEP
+
+)
+
+#%%
+with bd.BuildPart() as bone_part:
+    with bd.BuildSketch() as bone:
+        with bd.BuildLine() as ln:
+            bd.Polyline(
+                (0,0)
+                ,(0,2)
+                ,(-2,2)
+                ,(-4,4)
+                ,(-8,4)
+                ,(-8,0)
+                ,close=True
+            )
+            bd.mirror(ln.line, about=bd.Plane.YZ)
+        bd.make_face()
+        bd.mirror(bone.sketch, about=bd.Plane.XZ)
+    bd.extrude(amount=2)
+#%%
+e = plate_left.edges().sort_by(bd.Axis.X)[-1]
+print(plt_with_holes.center())
+show(
+    bone_part.part.translate(plt_with_holes.center()).translate((0,10,0))
+    ,bone_part.part.translate(plt_with_holes.center()).translate((0,-50,0))
+    ,plate_left
+    ,reset_camera=Camera.KEEP
+
+)
+#%%
+bx = bd.extrude(bd.Rectangle(15,20), amount=2).locate(bd.Location((-15/2,0,0)))-bd.offset(bone_part.part, amount=0.1)
+show(
+    #bone_part,
+    bx#.translate((-15/2,0,0))
+)
+#%%
+import os
+print(os.getcwd())
+bx.export_stl(os.getcwd()+"/joinery_plate.stl")
+bone_part.part.export_stl(os.getcwd()+"/joinery_joint.stl")
--- diode_1n4148.py
+++ diode_1n4148.py
@@ -1,20 +1,20 @@
 import build123d as bd
 
-def diode_1n4148(bent=True) -> bd.Part:
+def diode_1n4148(bent=True, leg_diameter=0.55) -> bd.Part:
     if bent:
         with bd.BuildPart() as prt:
             bd.Cylinder(radius=1.6/2, height=3.9)
             bd.fillet(prt.edges(), radius=0.2)
-            l = bd.Cylinder(radius=0.55/2, height=3.9+2*2)
+            l = bd.Cylinder(radius=leg_diameter/2, height=3.9+2*2)
             ends = l.faces().sort_by(bd.Axis.Z)
             with bd.Locations(
                 ends[0].center().to_tuple(),
                 ends[-1].center().to_tuple()
                 ):
-                bd.Sphere(radius=0.55/2)
+                bd.Sphere(radius=leg_diameter/2)
             with bd.BuildSketch(bd.Plane.XZ):
                 with bd.GridLocations(0, l.bounding_box().size.Z, 1, 2):
-                    bd.Circle(radius=0.55/2)
+                    bd.Circle(radius=leg_diameter/2)
             bd.extrude(amount=5)
         
         return prt.part
@@ -22,6 +22,6 @@ def diode_1n4148(bent=True) -> bd.Part:
         with bd.BuildPart() as prt:
             bd.Cylinder(radius=1.6/2, height=3.9)
             bd.fillet(prt.edges(), radius=0.2)
-            bd.Cylinder(radius=0.55/2, height=26*2+3.9)
+            bd.Cylinder(radius=leg_diameter/2, height=26*2+3.9)
         
         return prt.part
